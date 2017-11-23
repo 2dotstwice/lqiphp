856d76a831623925a40a86e9c34e51bc2b177277
Bert Ramakers <bertramakers@me.com>
2017-11-23 16:46:18 +0100

    Add gaussian blur post-processing strategy

---
+++ src/PostProcessing/GaussianBlurPostProcessingStrategy.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace TwoDotsTwice\LQIPHP\PostProcessing;
+
+class GaussianBlurPostProcessingStrategy implements PostProcessingStrategy
+{
+    /**
+     * @var int
+     */
+    private $deviation;
+
+    /**
+     * @var string
+     */
+    private $filterId;
+
+    public function __construct(
+        int $deviation = 12,
+        string $filterId = 'b'
+    ) {
+        $this->deviation = $deviation;
+        $this->filterId = $filterId;
+    }
+
+    public function process(string $svgContents): string
+    {
+        if ($this->deviation <= 0) {
+            return $svgContents;
+        }
+
+        $dom = new \DOMDocument();
+        $dom->loadXML($svgContents);
+
+        $blur = $dom->createElement('feGaussianBlur');
+        $blur->setAttribute('stdDeviation', $this->deviation);
+
+        $filter = $dom->createElement('filter');
+        $filter->setAttribute('id', $this->filterId);
+        $filter->appendChild($blur);
+
+        $svg = $dom->getElementsByTagName('svg')->item(0);
+        $svg->insertBefore($filter, $svg->firstChild);
+
+        $xpath = new \DOMXPath($dom);
+        $xpath->registerNamespace('svg', 'http://www.w3.org/2000/svg');
+        $gElements = $xpath->query('//svg:g');
+
+        $firstGroup = $gElements->item(0);
+        $firstGroup->setAttribute('filter', 'url(#' . $this->filterId . ')');
+        $firstGroup->setAttribute('fill-opacity', '0.5');
+
+        return $dom->saveXML();
+    }
+}
