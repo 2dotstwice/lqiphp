8e0348f8e774a37b38af0746c8372e7759976b07
Dmitriy Ivanenko <ivanenko.dmitrij@gmail.com>
2018-02-07 14:08:47 +0200

    unit test for the PrimitiveSVGGenerationStrategy

---
+++ tests/SVGGeneration/Unit/PrimitiveSVGGenerationStrategyTest.php
@@ -0,0 +1,41 @@
+<?php
+namespace TwoDotsTwice\LQIPHP\SVGGeneration;
+
+use PHPUnit\Framework\TestCase;
+use \TwoDotsTwice\LQIPHP\SVGGeneration\PrimitiveSVGGenerationStrategy as Strategy;
+
+function sys_get_temp_dir() {
+    return '/test';
+}
+
+function exec() {
+    return null;
+}
+
+function file_get_contents() {
+    return 'test';
+}
+
+function unlink() {
+    return true;
+}
+
+function uniqid() {
+    return '123';
+}
+
+class PrimitiveSVGGenerationStrategyTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_reads_a_file_runs_the_generation_and_returns_content()
+    {
+        $fileMock = $this->prophesize(\SplFileInfo::class);
+        $fileMock->getRealPath()->shouldBeCalled();
+        $file = $fileMock->reveal();
+
+        $process = new Strategy();
+        $this->assertEquals('test', $process->toSVG($file));
+    }
+}
